bf7f955d856e55d1d9de49f6a347595ac3fcfb94
W-Thiago <Thipsilva@outlook.com>
2022-10-12 15:40:46 -0300

    atletas-desafio CONCLUIDO

--- src/app/Program.java
+++ src/app/Program.java
@@ -10,11 +10,15 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         int atletas, i;
-        double som, count, countM, countH, porcentagemH, peso, Mediap, greatestHeight;
+        double som, countAlturaM, countM, countH, porcentagemH, peso, alturaMediaM, Mediap, greatestHeight, altura;
         String tallestAthlete = null;
 
+        countAlturaM = 0;
+        altura = 0;
+        alturaMediaM = 0;
+        countM = 0;
         porcentagemH = 0;
-        countH =0;
+        countH = 0;
         greatestHeight = 0;
         som = 0;
         peso = 0;
@@ -23,37 +27,25 @@ public class Program {
         System.out.print("Qual a quantidade de atletas? ");
         atletas = sc.nextInt();
         sc.nextLine();
-        //---------------------------------------------
-        for( i = 1; i <= atletas; i++ ) {
-            //---------------------------------------------
+
+        for (i = 1; i <= atletas; i++) {
             System.out.print("Digite os dados do atleta numero " + i + ":\n");
             System.out.print("Nome: ");
             String nome = sc.nextLine();
 
-
-
-
-            //---------------------------------------------
             System.out.print("sexo: ");
-            char sexo =sc.next().charAt(0);
+            char sexo = sc.next().charAt(0);
             while (sexo != 'F' && sexo != 'M') {
                 System.out.print("Valor invalido! Favor digitar F ou M :");
                 sexo = sc.next().charAt(0);
             }
-
-            if(sexo == 'M') {
-                countH+= + 1;
+            if (sexo == 'M') {
+                countH += +1;
                 porcentagemH = 100.0 * countH / atletas;
             }
 
-
-
-
-
-
-            //---------------------------------------------
             System.out.print("altura: ");
-            double altura = sc.nextDouble();
+            altura = sc.nextDouble();
 
             while (altura <= 0) {
                 System.out.print("Valor invalido! Favor digitar um valor positivo :");
@@ -64,11 +56,12 @@ public class Program {
                 tallestAthlete = nome;
             }
 
+            if (sexo == 'F') {
+                countAlturaM += altura;
+                countM += +1;
+                alturaMediaM = countAlturaM / countM;
+            }
 
-
-
-
-            //---------------------------------------------
             System.out.print("Peso: ");
             peso = sc.nextDouble();
             sc.nextLine();
@@ -77,22 +70,24 @@ public class Program {
                 System.out.print("Valor invalido! Favor digitar um valor positivo:");
                 peso = sc.nextDouble();
             }
-           if (peso > 0) {
+            if (peso > 0) {
                 som = som + peso;
-               Mediap = som / atletas;
+                Mediap = som / atletas;
             }
         }
 
         System.out.println("\n");
         System.out.println("RELATÓRIO:");
-        System.out.printf("Peso médio dos atletas: %.2f\n" , Mediap);
+        System.out.printf("Peso médio dos atletas: %.2f\n", Mediap);
         System.out.println("Atleta mais alto: " + tallestAthlete);
         System.out.printf("Porcentagem de homens: %.1f %%%n", porcentagemH);
 
-
-
-
+        if (countM == 0) {
+            System.out.println("Não há mulheres cadastradas ");
+        } else {
+            System.out.printf("Altura média das mulheres: %.2f\n ", alturaMediaM);
+        }
+        
         sc.close();
     }
-
 }
